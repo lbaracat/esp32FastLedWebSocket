d47e7c6dec6a627763729ccbd057a7c362c38c59
Baracat <lbaracat@gmail.com>
2023-10-13 00:10:35 -0300

    First FastFED test with taskCreate
    No JSON. Only a basic fastLED test. Create a new task exclusively
    for led control.

---
+++ server/ledstrip.h
@@ -0,0 +1,15 @@
+#define FASTLED_INTERNAL
+#include <FastLED.h>
+
+#ifndef _ledstrip_h
+#define _ledstrip_h
+
+#define STRIPTYPE APA102
+#define DATAPIN 14
+#define CLOCKPIN 13
+#define COLORSCHEME BGR
+#define NUMLEDS 193
+
+CRGB leds[NUMLEDS];
+
+#endif
--- server/server.ino
+++ server/server.ino
@@ -3,6 +3,7 @@
 #include <WebSocketsServer.h>
 #include <ArduinoJson.h>
 #include "cred.h"
+#include "ledstrip.h"
 
 #define DEBUG false
 
@@ -10,14 +11,15 @@ WebServer webServer(80);
 WebSocketsServer webSocket = WebSocketsServer(81);
 
 /*
-String webpage = "<!DOCTYPE html><html lang='en'><head></head><body><h1>-=( I'm on! )=-</h1><p>Take a random number:&nbsp;<span id='rand'>-</span></p><p><button type='button' id='btnSendBack'>Send info</button></p></body>\
-<script>var Socket;function init(){(Socket=new WebSocket('ws://'+window.location.hostname+':81/')).onmessage=function(n){processCommand(n)}}function processCommand(n){document.getElementById('rand').innerHTML=n.data,console.log(n.data)}\
-function btnSendBack(){Socket.send('Sending back some random stuff')}document.getElementById('btnSendBack').addEventListener('click',btnSendBack),window.onload=function(n){init()}</script></html>";
+String webpage = "<!DOCTYPE html><html lang='en'><head></head><body><h1>-=( I'm on! )=-</h1><p>Take my first random number:&nbsp;<span id='rand1'></span></p><p>Take my second random number:&nbsp;<span id='rand2'></span></p>\
+<p><button type='button' id='btnSendBack'>Send info</button></p></body><script>var Socket;function init(){(Socket=new WebSocket('ws://'+window.location.hostname+':81/')).onmessage=function(n){processCommand(n)}}function btnSendBack(){Socket.send(JSON.stringify({num_leds:'193',led_model:'APA102',color_mode:'BGR',clock_pin:'13',data_pin:'14'}))}\
+function processCommand(n){var e=JSON.parse(n.data);document.getElementById('rand1').innerHTML=e.rand1,document.getElementById('rand2').innerHTML=e.rand2,console.log(e.rand1),console.log(e.rand2)}document.getElementById('btnSendBack').addEventListener('click',btnSendBack),window.onload=function(n){init()}</script></html>";
 */
 
-String webpage = "<!DOCTYPE html><html lang='en'><head></head><body><h1>-=( I'm on! )=-</h1><p>Take my first random number:&nbsp;<span id='rand1'>-</span></p><p>Take my second random number:&nbsp;<span id='rand2'>-</span></p>\
-<p><button type='button' id='btnSendBack'>Send info</button></p></body><script>var Socket;function init(){(Socket=new WebSocket('ws://'+window.location.hostname+':81/')).onmessage=function(n){processCommand(n)}}function processCommand(n){var e=JSON.parse(n.data);\
-document.getElementById('rand1').innerHTML=e.rand1,document.getElementById('rand2').innerHTML=e.rand2,console.log(n.data),console.log(e.rand1),console.log(e.rand2)}function btnSendBack(){Socket.send(JSON.stringify({num_leds:'193',led_model:'APA102',color_mode:'BGR',clock_pin:'13',data_pin:'14'}))}document.getElementById('btnSendBack').addEventListener('click',btnSendBack),window.onload=function(n){init()}</script></html>";
+String webpage="<!DOCTYPE html><html lang='en'><head><meta charset='utf-8'><meta name='viewport' content='width=device-width,initial-scale=1'><title>Led Strip</title><link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css' integrity='sha256-MBffSnbbXwHCuZtgPYiwMQbfE7z+GOZ7fBPCNB06Z98=' crossorigin='anonymous'></head>\
+<body><div class='container'><div class='col'><div class='row'><h3>Led strip controller</h3></div><div class='row'><div class='input-group mb-3'><button class='btn btn-outline-secondary' type='button' id='btnSendBack'>Send to server</button><input type='text' class='form-control' placeholder='' aria-label='Example text with button addon' aria-describedby='btnSendBack' id='textToSend'></div>\
+</div></div></div></body><script>var Socket;function init(){(Socket=new WebSocket('ws://'+window.location.hostname+':81/')).onmessage=function(n){processCommand(n)}}function btnSendBack(){Socket.send(document.getElementById('textToSend').value)}function processCommand(n){console.log(n.data)}document.getElementById('btnSendBack').addEventListener('click',btnSendBack),window.onload=function(n){init()}</script>\
+<script src='https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js' integrity='sha256-YMa+wAM6QkVyz999odX7lPRxkoYAan8suedu4k2Zur8=' crossorigin='anonymous'></script></html>";
 
 // Variables for timer check
 unsigned long interval = 0; // in milliseconds
@@ -29,13 +31,23 @@ StaticJsonDocument<200> docTX;
 StaticJsonDocument<200> docRX;
 String jsonString = "";
 
+BaseType_t xReturned;
+TaskHandle_t xInitLedStripHnd = NULL;
+
 void setup() {
   Serial.begin(115200);
+  delay(1000);
+
   Serial.println("ESP32 ON.");
  
   initWiFi();
   initWebComponents();
   initTimer(1000);
+
+  xReturned = xTaskCreatePinnedToCore(initLedStrip, "LED Control", 1000, NULL, 0, &xInitLedStripHnd, tskNO_AFFINITY);
+  if (xReturned == pdPASS) {
+    Serial.println("Led task created.");
+  }
 }
  
 void loop() {
@@ -45,13 +57,17 @@ void loop() {
   now = millis();
   //Timer loop
   if (now - previousMillis > interval) {
+    jsonString = String(now);
+/*
     JsonObject object = docTX.to<JsonObject>();
     object["rand1"] = random(100);
     object["rand2"] = random(100);
     serializeJson(docTX, jsonString);
+*/
     webSocket.broadcastTXT(jsonString);
 
     if (DEBUG) Serial.println(jsonString);
+
     previousMillis += interval;
     jsonString = "";
   }
@@ -71,8 +87,8 @@ void initWiFi() {
     Serial.print(".");
   }
 
-  Serial.print(" CONNECTED!\n");
-  delay(4000);
+  Serial.println(" CONNECTED!");
+  delay(500);
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
 }
@@ -93,6 +109,21 @@ void initTimer(int i) {
   now = millis();
 }
 
+void initLedStrip(void *pvParameters) {
+  pinMode(CLOCKPIN, OUTPUT);
+  pinMode(DATAPIN, OUTPUT);
+  FastLED.addLeds<STRIPTYPE, DATAPIN, CLOCKPIN, COLORSCHEME>(leds, NUMLEDS);
+
+  while(1) {
+    for (int dot = 0; dot < NUMLEDS; dot+=2) {
+      leds[dot] = CRGB::Red;
+      FastLED.show();
+      leds[dot] = CRGB::Black;
+      delay(3);
+    }
+  }
+}
+
 void webSocketEvent(byte num, WStype_t type, uint8_t * payload, size_t length) {      // the parameters of this callback function are always the same -> num: id of the client who send the event, type: type of message, payload: actual data sent and length: length of payload
   switch (type) {                                     // switch on the type of information sent
     case WStype_DISCONNECTED:                         // if a client is disconnected, then type == WStype_DISCONNECTED
@@ -102,7 +133,12 @@ void webSocketEvent(byte num, WStype_t type, uint8_t * payload, size_t length) {
       Serial.println("Client " + String(num) + " connected");
       // optionally you can add code here what to do when connected
       break;
-    case WStype_TEXT:                                 // if a client has sent data, then type == WStype_TEXT
+    case WStype_TEXT:
+      for (int i=0; i<length; i++) {                  // print received data from client
+        Serial.print((char)payload[i]);
+      }
+      Serial.println("");                                 // if a client has sent data, then type == WStype_TEXT
+      /*
       DeserializationError err = deserializeJson(docRX, payload);
       if(err) {
         Serial.print("deserializeJson failed for payload ");
@@ -121,6 +157,7 @@ void webSocketEvent(byte num, WStype_t type, uint8_t * payload, size_t length) {
       Serial.println("Color mode: " + String(ls_color_mode));
       Serial.println(" Clock pin: " + String(ls_clock_pin));
       Serial.println("  Data pin: " + String(ls_data_pin));
+      */
 
       break;
   }
